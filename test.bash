548024e030b16d7973de3f8f87b70fbdadcb9c0c
Michael Schneider <michael.schneider@adtegrity.com>
2012-08-16 16:30:02 -0400

    Added script to run tests. All tests passing

---
+++ test.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# To regenerate this script:
+# find . | grep _test.go | xargs grep -n Test | grep -iv // | sed s/.*func/go\ test\ -run/ | sed s/\*testing.T\)\ {// | sed s/\(//
+
+go test -run TestGetAdgroupCriterion
+go test -run TestSetMaxCpm
+go test -run TestAddRemoveAdgroupCriterion
+go test -run TestGetAdgroup
+go test -run TestAddRemoveAdgroups
+go test -run TestGetCampaign
+go test -run TestDecodingSoapFault
+go test -run TestServicedAccountGet
